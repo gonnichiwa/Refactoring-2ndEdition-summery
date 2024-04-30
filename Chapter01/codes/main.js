9783384ec73dee8df15a2dafc466f9ecd69af0fc
gonnichiwa <keepthefaith00@gmail.com>
2024-04-30 16:49:47 +0900

    추출한 함수들 모두 statement() 블록 안으로 변경

--- Chapter01/codes/main.js
+++ Chapter01/codes/main.js
@@ -43,42 +43,46 @@ function statement(invoice) {
     result += `총액: ${format(totalAmount/100)}\n`;
     result += `적립 포인트 : ${volumeCredits}점 \n`;
     return result;
-}
+    
+    ///////
 
-function volumeCreditsFor(perf) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
-    return result;
-}
-
-// 공연 type별 요금계산
-function amountFor(aPerformance) {
-    let result = 0; // 명확한 이름으로 변경 : 함수의 결과값 변수 이름은 result
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000; // 기본료
-            if (aPerformance.audience > 20) { // 20명까진 기본이용
-                result += 10000 + 500 * (aPerformance.audience - 20); // 추가인원
-            }
-            result += 300 * aPerformance.audience; // comedy 특별추가요금
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    function volumeCreditsFor(perf) {
+        let result = 0;
+        result += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+        return result;
     }
-    return result;
-}
+    
+    // 공연 type별 요금계산
+    function amountFor(aPerformance) {
+        let result = 0; // 명확한 이름으로 변경 : 함수의 결과값 변수 이름은 result
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000; // 기본료
+                if (aPerformance.audience > 20) { // 20명까진 기본이용
+                    result += 10000 + 500 * (aPerformance.audience - 20); // 추가인원
+                }
+                result += 300 * aPerformance.audience; // comedy 특별추가요금
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+    
+    // 공연(performance) 제목(playID) 에 해당하는 공연정보(play) 를 가져옴
+    function playFor(aPerformance){
+        return plays[aPerformance.playID];
+    }
+}// statement
 
-// 공연(performance) 제목(playID) 에 해당하는 공연정보(play) 를 가져옴
-function playFor(aPerformance){
-    return plays[aPerformance.playID];
-}
+
 
 result = statement(invoice, plays);
 console.log(result);
